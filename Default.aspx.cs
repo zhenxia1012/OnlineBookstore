b48472d8c4a6ff703128d23ce1158f32446a6073
Zhen Xia <43132113+zhenxia1012@users.noreply.github.com>
2018-10-04 13:26:33 -0700

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    DataSet myDs = new DataSet();
+    DataView view = new DataView();
+    DataTable table = new DataTable();
+    SqlDataAdapter myDa;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            CataBind();
+        }
+
+        Address_Table();
+    }
+
+    public void CataBind()
+    {
+        string str = "select PName from [Province]";
+        SqlConnection sqlcon = GetConnection();
+        sqlcon.Open();
+        myDa = new SqlDataAdapter(str, sqlcon);
+        myDa.Fill(myDs);
+        sqlcon.Close();
+
+        ArrayList catalist = new ArrayList();
+        for (int i = 0; i < myDs.Tables[0].Rows.Count; i++)
+            catalist.Add(myDs.Tables[0].Rows[i]["PName"]);
+
+        this.Province_List.DataSource = catalist;
+        this.Province_List.DataBind();
+        myDs.Clear();
+    }
+
+    public void Address_Table()
+    {
+        string str1 = "select PName,CName from [City],[Province] where [City].ProvinceID = [Province].ID";
+        SqlConnection sqlcon = GetConnection();
+        sqlcon.Open();
+        myDa = new SqlDataAdapter(str1, sqlcon);
+        myDa.Fill(myDs);
+        sqlcon.Close();
+
+        view = myDs.Tables[0].DefaultView;
+    }
+
+    public SqlConnection GetConnection()
+    {
+        string str = "Server=(local);User Id=sa;Pwd=123;Database=bookstore";
+        SqlConnection con = new SqlConnection(str);
+        return con;
+    }
+
+    protected void Province_List_TextChanged(object sender, EventArgs e)
+    {
+        view.RowFilter = "PName='" + this.Province_List.Text + "'";
+        table = view.ToTable();
+        ArrayList catalist = new ArrayList();
+
+        for (int i = 0; i < table.Rows.Count; i++)
+            catalist.Add(table.Rows[i]["CName"]);
+
+        this.City_List.DataSource = catalist;
+        this.City_List.DataBind();
+    }
+}
---
+++ Default1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default1 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Page.DataBind();
+    }
+    protected void GO_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/unpaid1.aspx?id=20150713163744140");
+    }
+}
